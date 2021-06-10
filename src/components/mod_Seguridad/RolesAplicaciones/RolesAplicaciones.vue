fa6e3d2d7fdc2f8258274f5188d53995e9c8541f
David.Lipa <david.lipa@alphasys.com.bo>
2021-06-10 13:17:09 +0200

    modificaciones RoleAplicaciones

--- src/components/mod_Seguridad/RolesAplicaciones/RolesAplicaciones.vue
+++ src/components/mod_Seguridad/RolesAplicaciones/RolesAplicaciones.vue
@@ -1,9 +1,10 @@
 <template>
-	<v-card>
+	<v-card max-width="100%" style="margin:auto;">
 		<v-toolbar color="primary" style="color:white">
-			<v-toolbar-title>Datos de RolesAplicaciones</v-toolbar-title>
+			<v-toolbar-title class="font-large-color">Aplicacion de Roles</v-toolbar-title>
 			<v-divider></v-divider>
-			<v-text-field v-model="buscarrolesaplicaciones"
+			<v-text-field class="input-small"
+			        v-model="buscarrolesaplicaciones"
 					append-icon="search"
 					label="Buscar Registro"
 					single-line
@@ -23,21 +24,22 @@
 						class="elevation-1">
 			<template slot="item" slot-scope="props">
 				<tr>
+					
+					<td>{{ FormatRol(props.item.idrol) }}</td>
 					<td>{{ props.item.idrolesaplicaciones }}</td>
-					<td>{{ props.item.idrol }}</td>
 					<td>{{ props.item.idaplicacion }}</td>
 					<td>
 						<v-tooltip bottom>
 							<template v-slot:activator="{ on }">
 								<v-btn color="success" v-on="on" fab small dark  @click="Actualizar(props.item)"><v-icon>edit</v-icon></v-btn>
 							</template>
-							<span>Modificar Registro de Demo</span>
+							<span>Modificar Registro</span>
 						</v-tooltip>
 						<v-tooltip style="padding-left:10px" bottom>
 							<template v-slot:activator="{ on }" >
 								<v-btn color="error" v-on="on" fab small dark  @click="Eliminar(props.item)"><v-icon>delete</v-icon></v-btn>
 							</template>
-							<span>Eliminar Registro de Demo</span>
+							<span>Eliminar Registro</span>
 						</v-tooltip>
 					</td>
 				</tr>
@@ -45,9 +47,9 @@
 			<template v-slot:top>
 				<v-tooltip bottom>
 					<template v-slot:activator="{ on }">
-						<v-btn color="accent" v-on="on" @click="Insertar()">Adicionar Nuevo Registro de RolesAplicaciones</v-btn>
+						<v-btn class="btn-small-color" color="cyan" v-on="on" @click="Insertar()"><v-icon left>mdi-plus</v-icon>Agregar</v-btn>
 					</template>
-					<span>Adicionar nuevo registro de cliente</span>
+					<span>Adicionar nuevo registro</span>
 				</v-tooltip>
 			</template>
 			<template v-slot:no-data>
@@ -56,16 +58,19 @@
 				</v-alert>
 			</template>
 		</v-data-table>
-		<v-dialog v-model="dialog" persistent max-width="50%">
+		<v-dialog v-model="dialog" persistent max-width="45%">
 			<v-card>
 				<v-toolbar style="padding:10px" dark class="primary">
-					<v-toolbar-title>Datos de RolesAplicaciones</v-toolbar-title>
+					<v-toolbar-title>Aplicaciones de Roles</v-toolbar-title>
+					<v-spacer></v-spacer>
+					<v-btn color="error" fab dark small @click="Cancelar()">X</v-btn>
 				</v-toolbar>
 				<v-divider></v-divider>
-				<v-form ref="form" style="padding:10px">
+				<v-form ref="form" style="padding:10px" v-model="activa">
 					<v-card-text>
 						<v-layout wrap>
 							<template v-if="operacion == 'Insert'">
+								<!--
 								<v-flex sm12 style="padding: 5px">
 									<v-text-field v-model="rolesaplicaciones.idrolesaplicaciones"
 												label="IDRolesAplicaciones"
@@ -77,8 +82,10 @@
 												@input="rolesaplicaciones.idrolesaplicaciones = updateText(rolesaplicaciones.idrolesaplicaciones)">
 									</v-text-field>
 								</v-flex>
+								-->
 							</template>
 							<template v-else>
+								<!--
 								<v-flex sm12 style="padding: 5px">
 									<v-text-field v-model="rolesaplicaciones.idrolesaplicaciones"
 												label="IDRolesAplicaciones"
@@ -87,35 +94,42 @@
 												persistent-hint>
 									</v-text-field>
 								</v-flex>
+								-->
 							</template>
 							<v-flex sm12 style="padding: 5px">
-								<v-text-field v-model="rolesaplicaciones.idrol"
-											label="IDRol"
-											hint="Ingrese Nombre de IDRol"
-											placeholder="IDRol"
+								<v-autocomplete v-model="rolesaplicaciones.idrol"
+											label=" Seleccione Rol"
+											outlined
 											clearable
+											:items="lstrules"
+											item-text="descripcion"
+											item-value="idrol"
 											persistent-hint
-											required
+											:rules="validacion"
+											 no-data-text="No se encontro ningun datos"
 											@input="rolesaplicaciones.idrol = updateText(rolesaplicaciones.idrol)">
-								</v-text-field>
+								</v-autocomplete>
 							</v-flex>
 							<v-flex sm12 style="padding: 5px">
-								<v-text-field v-model="rolesaplicaciones.idaplicacion"
-											label="IDAplicacion"
-											hint="Ingrese Nombre de IDAplicacion"
-											placeholder="IDAplicacion"
+								<v-autocomplete v-model="rolesaplicaciones.idaplicacion"
+											label="Seleccione Aplicación"
+											outlined
 											clearable
 											persistent-hint
-											required
+											:items="lstaplicaciones"
+											item-text="nombre"
+											item-value="idaplicacion"
+											:rules="validacion"
+											 no-data-text="No se encontro ningun datos"
 											@input="rolesaplicaciones.idaplicacion = updateText(rolesaplicaciones.idaplicacion)">
-								</v-text-field>
+								</v-autocomplete>
 							</v-flex>
 						</v-layout>
 					</v-card-text>
 				</v-form>
 				<v-divider></v-divider>
 				<v-card-actions style="justify-content: center;padding:10px">
-					<v-btn color="success" dark style="width: 50%" @click="Grabar()">Grabar</v-btn>
+					<v-btn color="success" dark style="width: 50%" :disabled="!activa" @click="Grabar()">Grabar</v-btn>
 					<v-btn color="error" dark style="width: 50%" @click="Cancelar()">Cancelar</v-btn>
 				</v-card-actions>
 			</v-card>
@@ -124,3 +138,34 @@
 </template>
 
 <script src="./RolesAplicaciones.ts"></script>
+<style scoped>
+.font-extralarge-color {
+			font-size: 3em; /* tamaño de letra */ 
+			color: white; /* color de letra */  
+} 
+.font-large-color { 
+			font-size: 2.4em; /* tamaño de letra */ 
+			color: white; /* color de letra */ 
+} 
+.font-medium-color { 
+			font-size: 2em; /* tamaño de letra */ 
+			color: white; /* color de letra */ 
+} 
+.font-small-color { 
+			font-size: 1.4em; /* tamaño de letra */ 
+			color: white; /* color de letra */ 
+} 
+.btn-small-color{ 
+			font-size: 1.4em; /* tamaño de letra */ 
+			color: white; /* color de letra */ 
+} 
+.input-small{ 
+			font-size: 1.6em; /* tamaño de letra */ 
+} 
+.date-small{ 
+			font-size: 1.5em; /* tamaño de letra */ 
+} 
+.datatable-items-small{ 
+			font-size: 1.1em; /* tamaño de letra */ 
+} 
+</style> 
